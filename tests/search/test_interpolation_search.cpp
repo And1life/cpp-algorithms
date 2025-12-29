693be4381c01a7c0b3e9fc7dc03edb59b881d497
Anatoliy <and1life@yandex.ru>
2025-12-29 03:46:00 +0300

    test(interpolation_search): 100% coverage interpolation_search

--- tests/search/test_interpolation_search.cpp
+++ tests/search/test_interpolation_search.cpp
@@ -11,7 +11,7 @@ class InterpolationSearchTest : public ::testing::Test
 protected:
     std::vector<int> uniform_data{10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
     std::vector<int> skewed_data{1, 2, 3, 4, 5, 100, 200, 300, 400, 500};
-    std::vector<int> empty;
+    std::vector<int> empty_data;
     std::vector<int> single_data{42};
     std::vector<int> equal_data{55, 55, 55, 55, 55, 55, 55};
 
@@ -24,25 +24,33 @@ TEST_F(InterpolationSearchTest, UniformDataFound)
     EXPECT_TRUE(interpolation_search(uniform_data.begin(), uniform_data.end(), 100));
 }
 
-// TEST_F(BinarySearchTest, VectorNotFound) 
-// {
-//     EXPECT_FALSE(binary_search(vec.begin(), vec.end(), 0));
-//     EXPECT_FALSE(binary_search(vec.begin(), vec.end(), 4));
-//     EXPECT_FALSE(binary_search(vec.begin(), vec.end(), 10));
-// }
+TEST_F(InterpolationSearchTest, UniformDataNotFound)
+{
+    EXPECT_FALSE(interpolation_search(uniform_data.begin(), uniform_data.end(), 1));
+    EXPECT_FALSE(interpolation_search(uniform_data.begin(), uniform_data.end(), 45));
+    EXPECT_FALSE(interpolation_search(uniform_data.begin(), uniform_data.end(), 87));
+}
 
-// TEST_F(BinarySearchTest, ArrayAndDeque) 
-// {
-//     EXPECT_TRUE(binary_search(arr.begin(), arr.end(), 3));
-//     EXPECT_FALSE(binary_search(arr.begin(), arr.end(), 6));
+TEST_F(InterpolationSearchTest, SkewedDataFound)
+{
+    EXPECT_TRUE(interpolation_search(skewed_data.begin(), skewed_data.end(), 1));
+    EXPECT_TRUE(interpolation_search(skewed_data.begin(), skewed_data.end(), 100));
+    EXPECT_TRUE(interpolation_search(skewed_data.begin(), skewed_data.end(), 500));
+}
 
-//     EXPECT_TRUE(binary_search(dq.begin(), dq.end(), 20));
-//     EXPECT_FALSE(binary_search(dq.begin(), dq.end(), 25));
-// }
+TEST_F(InterpolationSearchTest, EmptyAndSingle) 
+{
+    EXPECT_FALSE(interpolation_search(empty_data.begin(), empty_data.end(), 42));
+    EXPECT_TRUE(interpolation_search(single_data.begin(), single_data.end(), 42));
+    EXPECT_FALSE(interpolation_search(single_data.begin(), single_data.end(), 0));
+}
 
-// TEST_F(BinarySearchTest, EmptyAndSingle) {
-//     EXPECT_FALSE(binary_search(empty.begin(), empty.end(), 42));
+TEST_F(InterpolationSearchTest, EqualDataFound)
+{
+    EXPECT_TRUE(interpolation_search(equal_data.begin(), equal_data.end(), 55));
+}
 
-//     EXPECT_TRUE(binary_search(single.begin(), single.end(), 42));
-//     EXPECT_FALSE(binary_search(single.begin(), single.end(), 0));
-// }
+TEST_F(InterpolationSearchTest, EqualDataNotFound)
+{
+    EXPECT_FALSE(interpolation_search(equal_data.begin(), equal_data.end(), 9));
+}
